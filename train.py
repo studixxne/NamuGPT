9e757495aa6ce708447238471c9bc5fdfce784bc
seongone <studioone@sogang.ac.kr>
2026-05-12 23:12:52 +0900

    TrainConfig 구현 및 Cfg 기반으로 함수 구조 수정

--- train.py
+++ train.py
@@ -1,9 +1,22 @@
 import math
 import torch
+from dataclasses import dataclass
 from torch.utils.data import Dataset, DataLoader
 from transformers import PreTrainedTokenizerFast
 
-def configure_optimizer(model, lr, weight_decay=0.1, betas=(0.9, 0.95)):
+@dataclass
+class TrainConfig:
+    batch_size:    int   = 8
+    epochs:        int   = 3
+    max_lr:        float = 1e-4
+    weight_decay:  float = 0.1
+    betas:         tuple = (0.9, 0.95)
+    warmup_steps:  int   = 100
+    min_lr_ratio:  float = 0.1
+    grad_clip:     float = 1.0
+    log_interval:  int   = 10
+
+def configure_optimizer(model, train_cfg: TrainConfig):
     """
     [GPT를 더 효율적으로 학습하기 위해 Optimizer를 설정]
 
@@ -39,7 +52,7 @@ def configure_optimizer(model, lr, weight_decay=0.1, betas=(0.9, 0.95)):
             no_decay.append(param)
     
     param_groups = [
-        {'params': decay, 'weight_decay': weight_decay},
+        {'params': decay,    'weight_decay': train_cfg.weight_decay},
         {'params': no_decay, 'weight_decay': 0}
     ]
 
@@ -47,10 +60,10 @@ def configure_optimizer(model, lr, weight_decay=0.1, betas=(0.9, 0.95)):
     # Kernel Fusion은 필요한 연산들을 하나의 커널로 통합해서 GPU에서의 VRAM과 코어 사이의 이동 횟수를 줄여주는 하드웨어 최적화 기법이다.
     # 트레이드 오프 없이 효율성 증대가 크기 때문에 사용이 가능하다면 반드시 사용하는 것이 좋다!
     use_fused = torch.cuda.is_available()
-    optimizer = torch.optim.AdamW(param_groups, lr=lr, betas=betas, fused=use_fused)
+    optimizer = torch.optim.AdamW(param_groups, lr=train_cfg.max_lr, betas=train_cfg.betas, fused=use_fused)
     return optimizer
 
-def get_lr_scheduler(optimizer, warmup_steps, total_steps, min_lr_ratio=0.1):
+def get_lr_scheduler(optimizer, total_steps, train_cfg: TrainConfig):
     """
         Cosine Decay를 바로 적용하기 이전에 Warmup을 적용함으로써 lr을 작게 설정해 초기 학습을 안정화시켜준다.
 
@@ -64,20 +77,20 @@ def get_lr_scheduler(optimizer, warmup_steps, total_steps, min_lr_ratio=0.1):
     def lr_lambda(step):
         # Warmup 구간 (step < warmup_steps)
         # step이 0에서 warmup_steps로 올라가면서 ratio가 0.0에서 1.0으로 선형적으로 증가하게 된다.
-        if step < warmup_steps:
-            return step / warmup_steps
-        
+        if step < train_cfg.warmup_steps:
+            return step / train_cfg.warmup_steps
+
         # 총 학습 스텝이 모든 끝난 경우, 즉 추가 학습 시에는 미세 조정을 위해 최소 비율을 적용한다.
         if step > total_steps:
-            return min_lr_ratio
-        
+            return train_cfg.min_lr_ratio
+
         # Warmup 구간 <= step <= total_steps 일 때는 Cosine Decay를 적용한다.
         # 막 Warmup이 끝났을 때에는 진척도가 0.0이며, 전체 학습이 끝나가는 시점에는 진척도가 1.0이 된다.
-        progress = (step - warmup_steps) / (total_steps - warmup_steps)
-        
+        progress = (step - train_cfg.warmup_steps) / (total_steps - train_cfg.warmup_steps)
+
         # progress는 [0, 1]임으로 cos은 [-1, 1]이 된다.
         # 최소 학습 비율 이상을 반드시 유지하되 진척도가 커질수록 ratio가 1에서부터 시작하여 천천히 감소하도록 한다.
-        ratio = min_lr_ratio + 0.5 * (1 - min_lr_ratio) * (1 + math.cos(math.pi * progress))
+        ratio = train_cfg.min_lr_ratio + 0.5 * (1 - train_cfg.min_lr_ratio) * (1 + math.cos(math.pi * progress))
         return ratio
     
     # 매 스텝마다 optimizer의 정해진 lr에 lr_lambda로 얻어진 ratio를 곱해서 구한 lr로 학습을 진행시키는 Scheduler
@@ -98,11 +111,11 @@ class GPTDataset(Dataset):
         y = data[1:]
         return x, y
 
-def train(model, data_loader, optimizer, scheduler, device, epochs, total_steps, grad_clip=1.0, log_interval=10):
+def train(model, data_loader, optimizer, scheduler, device, total_steps, train_cfg: TrainConfig):
     model.train()
     step = 0
 
-    for epoch in range(epochs):
+    for epoch in range(train_cfg.epochs):
         for x, y in data_loader:
             x, y = x.to(device), y.to(device)
 
@@ -114,38 +127,34 @@ def train(model, data_loader, optimizer, scheduler, device, epochs, total_steps,
             # 뒤에 _가 붙으면 inplace 연산
             # loss가 튈 때 gradient가 폭발하여 학습이 망가질 수 있음으로 이를 방지하기 위해서 Grad Cliping 도입
             # 만약 L2 Norm이 5.0 이라면 grad_clip이 1.0이므로 모든 기울기에 0.2를 곱함으로써 L2 Norm을 1로 맞춰준다
-            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), train_cfg.grad_clip)
 
             optimizer.step()
             scheduler.step()
 
             step += 1
-            if step % log_interval == 0:
+            if step % train_cfg.log_interval == 0:
                 current_lr = scheduler.get_last_lr()[0]
-                print(f'epoch [{epoch+1}|{epochs}] | step [{step}|{total_steps}] | loss {loss.item():.4f} | current_lr {current_lr:.2e}')
+                print(f'epoch [{epoch+1}|{train_cfg.epochs}] | step [{step}|{total_steps}] | loss {loss.item():.4f} | current_lr {current_lr:.2e}')
 
 if __name__ == '__main__':
     from model import NanoGPT, GPTConfig
 
     # *=============================================*
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2')
+    device     = 'cuda' if torch.cuda.is_available() else 'cpu'
+    tokenizer  = PreTrainedTokenizerFast.from_pretrained('skt/kogpt2-base-v2')
+    text       = '안녕하세요? 두쫀쿠 좋아하세요? 저는 두쫀쿠를 먹어봤는데 그냥 크런키 맛나는데 이걸 8000원 넘게 줘가면서 먹을 필요가 있을까요?'
 
-    config = GPTConfig()
-    block_size = config.block_size
-    batch_size = 8
-    epochs = 3
-    max_lr = 1e-4
-    warmup_steps = 100
-    text = '안녕하세요? 두쫀쿠 좋아하세요? 저는 두쫀쿠를 먹어봤는데 그냥 크런키 맛나는데 이걸 8000원 넘게 줘가면서 먹을 필요가 있을까요?'
+    model_cfg = GPTConfig()
+    train_cfg = TrainConfig()
     # *=============================================*
 
-    dataset = GPTDataset(text, tokenizer, block_size)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    total_steps = epochs * len(data_loader)
+    dataset     = GPTDataset(text, tokenizer, model_cfg.block_size)
+    data_loader = DataLoader(dataset, batch_size=train_cfg.batch_size, shuffle=True)
+    total_steps = train_cfg.epochs * len(data_loader)
 
-    model = NanoGPT(config).to(device)
-    optimizer = configure_optimizer(model, lr=max_lr)
-    scheduler = get_lr_scheduler(optimizer, warmup_steps, total_steps)
+    model     = NanoGPT(model_cfg).to(device)
+    optimizer = configure_optimizer(model, train_cfg)
+    scheduler = get_lr_scheduler(optimizer, total_steps, train_cfg)
 
-    train(model, data_loader, optimizer, scheduler, device, epochs, total_steps)
+    train(model, data_loader, optimizer, scheduler, device, total_steps, train_cfg)
